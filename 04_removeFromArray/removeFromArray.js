44d0d9e2ab26c153ac950a58d12f282c9af0900e
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2025-03-02 08:38:48 +0000

    completed using splice method; fiter can be used, which is much simpler

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  args.forEach((arg) => {
+    let i = 0;
+    while (i < arr.length) {
+      //cycle through array to find the rmv variable
+      if (arr[i] === arg) {
+        arr.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+  });
+  return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//can also use filter instead of splice

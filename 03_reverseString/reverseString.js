0d93cc02117f456d702008c2db73529ddc1e8a5b
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2025-03-02 08:10:33 +0000

    complet

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = [];
+  const splitString = string.split('');
+  //get the string and split
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString.push(splitString[i]);
+  }
+  let result = reversedString.join('');
+  return result;
+  // reverse string
+  // join back together
 };
 
 // Do not edit below this line

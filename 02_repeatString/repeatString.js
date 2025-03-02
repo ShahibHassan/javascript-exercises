b065df68b01e287bb451d3a55efb417bc44790c6
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2025-03-02 07:32:11 +0000

    complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
 
+  let string = '';
+
+  for (let i = 0; i < num; i++) {
+    string += word;
+  }
+
+  return string;
 };
 
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('odin');
 // Do not edit below this line
 module.exports = repeatString;

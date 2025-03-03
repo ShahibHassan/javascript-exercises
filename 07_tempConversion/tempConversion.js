60fe6a8461d1be96e4f3c9fc888ce664bfaf3f99
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2025-03-03 07:02:25 +0000

    complete using toFixed method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const celcius = (temp - 32) * (5 / 9);
+  return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const fahrenheit = temp * (9 / 5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
